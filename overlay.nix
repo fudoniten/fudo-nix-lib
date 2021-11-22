252346a8e8267079eefe2cd595a89861c4b67c5d
niten <niten@fudo.org>
2021-11-22 06:26:06 -0800

    Split out lib.nix

--- overlay.nix
+++ overlay.nix
@@ -1,14 +1,6 @@
 (final: prev: with builtins; {
   lib = let
     pkgs = prev;
-    lib = prev.lib;
   in
-    lib // {
-      ip = import ./lib/ip.nix { inherit pkgs; };
-      dns = import ./lib/dns.nix { inherit pkgs; };
-      passwd = import ./lib/passwd.nix { inherit pkgs; };
-      lisp = import ./lib/lisp.nix { inherit pkgs; };
-      network = import ./lib/network.nix { inherit pkgs; };
-      fs = import ./lib/filesystem.nix { inherit pkgs; };
-    };
+    pkgs.lib // (import ./lib.nix { inherit pkgs; });
 })

65f7f1c5425d775ac56a5cdc9555ff05628d0049
niten <niten@fudo.org>
2023-06-27 16:23:27 -0700

    Switch to python3 for json formatting

--- lib/lib/text.nix
+++ lib/lib/text.nix
@@ -5,13 +5,12 @@ let
   get-basename-without-hash = filename:
     head (builtins.match "^[a-zA-Z0-9]+-(.+)$" (baseNameOf filename));
 
-  format-json-file = filename: pkgs.stdenv.mkDerivation {
-    name = "formatted-${get-basename-without-hash filename}";
-    phases = [ "installPhase" ];
-    buildInputs = with pkgs; [ python ];
-    installPhase = "python -mjson.tool ${filename} > $out";
-  };
+  format-json-file = filename:
+    pkgs.stdenv.mkDerivation {
+      name = "formatted-${get-basename-without-hash filename}";
+      phases = [ "installPhase" ];
+      buildInputs = with pkgs; [ python3 ];
+      installPhase = "python -mjson.tool ${filename} > $out";
+    };
 
-in {
-  inherit format-json-file;
-}
+in { inherit format-json-file; }
